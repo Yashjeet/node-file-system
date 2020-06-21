9fd1205c2681ee694c2d0202d7cde868d2c04b04
yashjeet <luthra.yashjeet@gmail.com>
2020-06-21 14:56:32 +0530

    added upload file

--- index.js
+++ index.js
@@ -2,26 +2,45 @@ const express = require('express');
 const app = express();
 const port = 3000
 const fs = require('fs')
+const fileUpload = require('express-fileupload');
+
+app.use(fileUpload())
 
 app.get('/read-file', (req, res, next) => {
 
-    fs.readFile('./sample.txt', 'utf8', (error, result) => {
+
+});
+
+app.post('/upload-file', (req, res, next) => {
+
+    const dir = __dirname + '/upload';
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    const file = req.files.file;
+    fs.writeFile(`./upload/${file.name}`, file.data, 'utf8', (error, writeResult) => {
         if (error) {
-            res.json({
+            res.send({
                 status: false,
-                message: "Failed  to read file",
-                error
+                message: 'File upload failed'
             })
         }
         else {
-            res.json({
-                status: true,
-                message: "Successfully get file content",
-                entity: result
+            fs.readFile(`./upload/${file.name}`, 'utf8', (readFileError, result) => {
+                if (readFileError) {
+                    res.send({
+                        status: false,
+                        message: 'File read failed'
+                    })
+                }
+                res.json({
+                    status: true,
+                    message: "File upload successfully!",
+                    entity: result
+                })
             })
         }
-    });
-
+    })
 });
 
 app.listen(port, () => console.log(`server listening to port : ${port}`));
